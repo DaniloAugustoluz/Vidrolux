796bdab6b5117a467b377d647d63c727a5c9a96d
DaniloAugustoluz <daniloaugusto.lluz@gmail.com>
2025-05-27 17:31:38 -0300

    nova pagina de servicos

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ closeModal.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
+
+
+/*  COMPONENTE CARD RESIDENCIAL   */
+const openCardResidencial = document.getElementById("residencial");
+const cardResidencial = document.getElementById("card__residencial");
+
+openCardResidencial.addEventListener("click", () => {
+    cardResidencial.style.display = "flex";
+    cardComercial.style.display = "none";
+});
+
+/*  COMPONENTE CARD COMERCIAL   */
+const openCardComercial = document.getElementById("comercial");
+const cardComercial = document.getElementById("card__comercial");
+
+openCardComercial.addEventListener("click", () => {
+    cardResidencial.style.display = "none";
+    cardComercial.style.display = "flex";
+});
